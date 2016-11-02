aa5819f766cf0282851b6d7a1a442b3fd67e57af
glottotopia <sebastian.nordhoff@glottotopia.de>
2016-11-02 22:07:00 +0100

    first version with output

---
+++ examples/coastriverborder.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Purpose:      create a map with political boundaries and rivers
+#
+# GMT modules:  pscoast, pslegend
+
+filename=$1
+west=$2
+east=$3
+south=$4
+north=$5
+
+echo $filename $west $east $south $north end
+
+GMT pscoast -R$west/$east/$south/$north \
+              -Jm.5c \
+              -I1/1p,lightblue  -I2/0.25p,lightblue \
+              -N1/.5p,-  \
+              -Gwhite  \
+              -Slightblue   > $filename.ps
+              
+ps2pdf -dEPSCrop $filename.ps 
+pdf2svg $filename.pdf $filename.svg
+
+
